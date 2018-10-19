26637858284682b42c2c00e41a9b8c96ee300cae
Local Jhost <localsnet@gmail.com>
2018-10-19 18:04:11 +0300

    last changes

--- PMC/app1/app81.py
+++ PMC/app1/app81.py
@@ -1,4 +1,6 @@
 import json
+#85 introduction to get_close_matches
+from difflib import get_close_matches
 filename='data.json'
 #PCC way
 #with open(filename) as f_obj:
@@ -11,12 +13,22 @@ word = input('Type word for definition: ')
 
 def wordfind(w):
     '''Find definition by word'''
-# 83 Implementing Case Sensitivity
+#83 Implementing Case Sensitivity
     w = w.lower()
 #82 Accounting for non-existing Words
+    # check type of data
+    print(type(data))
+    print(type(data.keys()))
+    print(type((data[w])))
+
     if w in data:
         print(data[w])
+
     else:
-        print('The word not found, check spelling')
+        print(get_close_matches(w, data.keys()))# > 0:
+
+
+
+
 
 wordfind(word)
