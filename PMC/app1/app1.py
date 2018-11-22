6bd1bc64421b29e505d0965244845aaff7fa85c1
Local Jhost <localsnet@gmail.com>
2018-11-22 19:05:12 +0300

    Bug fix1 done in app81 and trying title add in get_close_match conditional block

--- PMC/app1/app1.py
+++ PMC/app1/app1.py
@@ -3,20 +3,35 @@ from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
+
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:  # if user entered "texas" this will check for "Texas" as well.
+        return data[w.title()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
             return data[get_close_matches(w, data.keys())[0]]
         elif yn == "N":
-            return "The word doesn't exist. Please double check it."
+            return False #"The word doesn't exist. Please double check it."
+         # Title check
+        elif len(get_close_matches(w.title(), data.keys())) > 0:
+            yn = input(
+                "Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w.title(), data.keys())[0])
+            if yn == "Y":
+                return data[get_close_matches(w.title(), data.keys())[0]]
+            elif yn == "N":
+                return "The word doesn't exist. Please double check it."
+
+            else:
+                return "The word doesn't exist. Please double check it."
+
         else:
             return "We didn't understand your entry."
-    else:
-        return "The word doesn't exist. Please double check it."
+
+
 
 word = input("Enter word: ")
 output = translate(word)
--- PMC/app1/app81.py
+++ PMC/app1/app81.py
@@ -1,37 +1,49 @@
+# Bug task1 Please add another conditional block to the program so that the program returns the definition of names
+# that start with a capital letter.
 import json
-#85 introduction to get_close_matches
+# 85 introduction to get_close_matches
 from difflib import get_close_matches
-filename='data.json'
-#PCC way
-#with open(filename) as f_obj:
+
+filename = 'data.json'
+# PCC way
+# with open(filename) as f_obj:
 # data=json.load(f_obj)
-#PMC way
+# PMC way
 data = json.load(open(filename))
-#Check some key
-#print(data["sky"])
+# Check some key
+# print(data["sky"])
 word = input('Type word for definition: ')
+
 
 def wordfind(w):
     '''Find definition by word'''
-#83 Implementing Case Sensitivity
+    # 83 Implementing Case Sensitivity
     w = w.lower()
-#82 Accounting for non-existing Words
+    print (w)
+
     # check type of data
     print(type(data))
     print(type(data.keys()))
-#The positive result is 3 according default ratio of get_close_matches
-    print(len(get_close_matches(w, data.keys())))
-#Only for existed keys    print(type((data[w])))
-
+    #print(type(get_close_matches(w, data.keys())[0]))
+    # Only for existed keys    print(type((data[w])))
+    # First we expect correct word by checking it in dictionary keys, otherwise try to find it with get_close_matches in elif block
     if w in data:
         return data[w]
+    elif w.title() in data:
+        return data[w.title()]
 
+
+
+    # 82 Accounting for non-existing Words. i.e. typos
+    # The positive result is 3 according default ratio of get_close_matches
     elif len(get_close_matches(w, data.keys())) > 0:
 
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
 
         if yn == "Y":
-
+            # get_close_matches returns a list data with 3 items but an index 0 return a one word (string) of all  (is most close to desired word) and it was passed
+            # passed as a dict key (argument) to the data dictionary which in turn will return its  value (list data type,
+            # because a values stored in this way in current json file)   eventually.
             return data[get_close_matches(w, data.keys())[0]]
 
         elif yn == "N":
@@ -41,19 +53,19 @@ def wordfind(w):
         else:
 
             return "We didn't understand your entry."
-
+    # Counting for Non-existing Words
     else:
 
         return "The word doesn't exist. Please double check it."
 
 
-
-
 output = wordfind(word)
+# check data type
 print(type(output))
 if type(output) == list:
-# So if was "N" it's a string and else block is the case
+    # Formatting the user's output in readable view (list to string)
     for item in output:
         print(item)
+# So if was "N" it's a string and else block is the case
 else:
     print(output)
